e8e21a5df40a5028cacbc0b7b8613c8802b97ec4
Colten Glover <coltenglover@yahoo.com>
2023-08-07 11:15:47 -0400

    Finished exercise 3

---
+++ src/main/java/OracleCollectionsExercises/Exercise3.java
@@ -0,0 +1,25 @@
+package OracleCollectionsExercises;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Exercise3 {
+//    Write a method that takes a List<String> and applies String.trim to each element.
+    public static void trimStrings(List<String> list) {
+        ListIterator<String> lit = list.listIterator();
+        while (lit.hasNext()) {
+            lit.set(lit.next().trim());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("Colten     ");
+        list.add("Mochi");
+        System.out.printf("Before trimming: %s\n", list);
+        trimStrings(list);
+        System.out.printf("After trimming: %s\n", list);
+    }
+}
