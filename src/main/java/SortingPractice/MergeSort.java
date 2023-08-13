1ddec3f828f375bf74922ef8d7aafb2c6bbac04f
Colten Glover <coltenglover@yahoo.com>
2023-08-13 09:15:13 -0400

    Finished merge sort

--- src/main/java/SortingPractice/MergeSort.java
+++ src/main/java/SortingPractice/MergeSort.java
@@ -7,14 +7,17 @@ public class MergeSort {
     public static void main(String[] args) {
         //Generate 1,000,000 random integers
         Random r = new Random();
-        int[] arr = new int[10];
+        int[] arr = new int[100000000];
         for(int i = 0; i < arr.length; i++) {
-            arr[i] = r.nextInt(10);
+            arr[i] = r.nextInt(100000000);
         }
 
-        System.out.printf("Before: %s\n", Arrays.toString(arr));
+        //System.out.printf("Before: %s\n", Arrays.toString(arr));
+        long start = System.currentTimeMillis();
         mergeSort(arr);
+        long end = System.currentTimeMillis();
         //System.out.printf("After: %s\n", Arrays.toString(arr));
+        System.out.printf("Time: %d\n", end - start);
     }
 
     static void mergeSort(int[] arr) {
@@ -30,20 +33,46 @@ public class MergeSort {
         int[] right = new int[arr.length - mid];
 
         //Copy elements into their respective halves
-        System.out.printf("Left: ");
         for (int i = 0; i < mid; i++) {
-            System.out.printf("%d ", arr[i]);
             left[i] = arr[i];
         }
-        System.out.print("\nRight: ");
         for (int i = mid; i < arr.length; i++) {
-            System.out.printf("%d ", arr[i]);
             right[i - mid] = arr[i];
         }
-        System.out.println();
+
+        //Merge Sort both halves
+        mergeSort(right);
+        mergeSort(left);
+        //Merge 2 halves
+        merge(arr, left, right);
     }
 
     static void merge(int[] arr, int[] left, int[] right) {
+        int rightIndex = 0;
+        int leftIndex = 0;
+        int arrIndex = 0; //Tracks input array position
 
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] <= right[rightIndex]) {
+                arr[arrIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                arr[arrIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            arrIndex++;
+        }
+
+        while (leftIndex < left.length) {
+            arr[arrIndex] = left[leftIndex];
+            arrIndex++;
+            leftIndex++;
+        }
+
+        while (rightIndex < right.length) {
+            arr[arrIndex] = right[rightIndex];
+            arrIndex++;
+            rightIndex++;
+        }
     }
 }
